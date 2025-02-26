2a0077ce6e1a6bab347b18b93cc325d26ee7b91a
Vital9i <vitalikdoiniak@gmail.com>
2025-02-26 23:20:50 +0300

    start project

---
+++ src/Components/Button.tsx
@@ -0,0 +1,10 @@
+type ButtonProps = {
+    title: string
+    onClick?: () => void
+};
+
+export const Button = ({ title, onClick }: ButtonProps) => {
+    return (
+        <button onClick={onClick}>{title}</button>
+    );
+};
